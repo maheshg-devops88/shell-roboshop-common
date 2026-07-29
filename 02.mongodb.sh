f3444a7bf02575e90e4704573e43194d60b47449
mahesh <mahesh050917@gmail.com>
2026-07-29 06:23:59 +0530

    added roboshop common shell scripts

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -8,29 +8,3 @@ LOG_FILE=/var/log/shell-roboshop/$0.log
 
 root_check()
 
-VALIDATE() {
-if [ $1 == 0 ]; then
-   echo "$2.....Success" 
- else
-   echo "$2.....Failure"
-fi
-}
-
-mkdir -p $LOG_FOLDER
-VALIDATE $? "LOG directory creation "
-
-cp mongo.repo /etc/yum.repos.d/
-VALIDATE $? "Copy Process"
-
-dnf install mongodb-org -y  &>> $LOG_FILE
-VALIDATE $? "mongodb Installation"
-
-systemctl enable mongod 
-VALIDATE $? "mongodb enabled"
-
-
-sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf  &>> $LOG_FILE
-VALIDATE $? "Changing to 127.0.0.1 to 0.0.0.0" 
-
-systemctl start mongod
-VALIDATE $? "mongodb service start state"
