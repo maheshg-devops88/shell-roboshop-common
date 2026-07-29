84d9f2d67e48c8c4044bd02f569341ea89c76972
mahesh <mahesh9.storage@gmail.com>
2026-07-29 20:25:11 +0530

    added common roboshop scripts

--- 01.common.sh
+++ 01.common.sh
@@ -56,9 +56,59 @@ Redis_Installation () {
 
 Service_Enable() {
 
+  systemctl daemon-reload
+  VALIDATE $? "$SERVICE Service Daemon reload"
+
   systemctl enable $SERVICE &>> $LOG_FILE
   VALIDATE $? "Enable Systemctl service $SERVICE"
 
   systemctl start $SERVICE &>> $LOG_FILE
   VALIDATE $? "Start Systemctl service $SERVICE"
+}
+
+nodejs_install() {
+
+dnf module disable nodejs -y &>> $LOG_FILE
+VALIDATE $? "Disable Module nodejs"
+
+dnf module enable nodejs:20 -y &>> $LOG_FILE
+VALIDATE $? "Enable module nodejs 20"
+
+dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? "Install nodejs"
+}
+
+
+roboshop_user() {
+
+id roboshop &>> $LOG_FILE
+if [ $? == 1 ]; then
+    
+    echo "Create roboshop user...."
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
+  else
+    echo "Roboshop user already exists.."
+fi
+}
+
+app_install() {
+
+rm -rf /app
+VALIDATE $? "remove /app Dir if exists"
+
+mkdir -p /app
+VALIDATE $? "created directory /app"
+
+curl -o /tmp/$SERVICE.zip https://roboshop-artifacts.s3.amazonaws.com/$SERVICE-v3.zip &>> $LOG_FILE
+VALIDATE $? "download $SERVICE.zip file to tmp Dir"
+
+cd /app
+unzip /tmp/$SERVICE.zip &>> $LOG_FILE
+VALIDATE $? "unzip $SERVICE.zip to /app"
+
+npm install &>> $LOG_FILE
+VALIDATE $? "Install dependencies"
+
+cp $WRK_DIR/$SERVICE.service /etc/systemd/system/
+VALIDATE $? "Copy $SERVICE.service to /etc/systemd/system/"
 }
---
+++ 05.user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./01.common.sh
+
+LOG_FOLDER=/var/log/shell-roboshop
+LOG_FILE=/var/log/shell-roboshop/$0.log
+WRK_DIR=$PWD
+SERVICE=user
+
+root_check
+
+VALIDATE()
+if [ $1 == 0 ]; then
+   echo "$2.....Success" 
+ else
+   echo "$2.....Failure"
+fi
+
+LOG_DIR
+
+nodejs_install
+
+roboshop_user
+
+Service_Enable
+
+app_install
