c41950474481f600caf8ed442c95d8ee635660ca
mahesh <mahesh9.storage@gmail.com>
2026-07-29 20:30:57 +0530

    added common roboshop scripts

---
+++ 06.cart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./01.common.sh
+
+LOG_FOLDER=/var/log/shell-roboshop
+LOG_FILE=/var/log/shell-roboshop/$0.log
+WRK_DIR=$PWD
+SERVICE=cart
+
+root_check
+
+VALIDATE()
+if [ $1 == 0 ]; then
+   echo "$2.....Success" 
+ else
+   echo "$2.....Failure"
+fi
+
+LOG_DIR
+
+nodejs_install
+
+roboshop_user
+
+app_install
+
+Service_Enable
