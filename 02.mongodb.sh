1e817ddd4c9522d71a2b77c1d8cb17988ab376d0
mahesh <mahesh050917@gmail.com>
2026-07-30 09:34:40 +0530

    added roboshop common shell scripts

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -6,7 +6,13 @@ LOG_FILE=/var/log/shell-roboshop/$0.log
 
 root_check
 
-VALIDATE
+VALIDATE() {
+if [ $1 -eq 0 ]; then
+   echo "$2.....Success" 
+ else
+   echo "$2.....Failure"
+fi
+}
 
 LOG_DIR
 
