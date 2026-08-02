a38805b1794b7b231e37872405bb74e54ad6adc7
mahesh <mahesh050917@gmail.com>
2026-08-02 14:10:21 +0530

    added roboshop common shell scripts

--- 04.frontend.sh
+++ 04.frontend.sh
@@ -6,19 +6,8 @@ LOG_FILE=/var/log/shell-roboshop/$0.log
 WRK_DIR=$PWD
 SERVICE=nginx
 
-
-
 root_check
-
-VALIDATE()
-if [ $1 == 0 ]; then
-   echo "$2.....Success" 
- else
-   echo "$2.....Failure"
-fi
-
 LOG_DIR
-
 nginx_install
 
 rm -rf /usr/share/$SERVICE/html/*  &>> $LOG_FILE
